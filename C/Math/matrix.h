cffba99a5cec98ce4ff5d8b1ccc58316e4a5efd2
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-07 23:59:11 -0300

    Redefine return type for existing functions and add new functions

--- C/Math/matrix.h
+++ C/Math/matrix.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-float* matrix_sum(float *array1, float *array2);
-float* matrix_scalar_product(float *array1, int scalar);
-float* matrix_transposition(float *array);
-float* matrix_product(float *array1, float *array2);
+int get_columns(float **array); 
+	/* Returns the number of columns in the matrix  */
+int get_rows(float **array);
+int same_order_matrices(float **array1, float *array2);
+float** matrix_sum(float **array1, float **array2);
+float** matrix_scalar_product(float **array, int scalar);
+float** matrix_transposition(float **array);
+float** matrix_product(float **array1, float **array2);

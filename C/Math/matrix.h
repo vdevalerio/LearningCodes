7e5571b38b9bb8b99d05afa8d470ba55cecb9877
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-08 00:07:30 -0300

    Refactor functions prototypes

--- C/Math/matrix.h
+++ C/Math/matrix.h
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int get_columns(float **array); 
-	/* Returns the number of columns in the matrix  */
-int get_rows(float **array);
+int matrix_columns(float **array);
+int matrix_rows(float **array);
 int same_order_matrices(float **array1, float *array2);
 float** matrix_sum(float **array1, float **array2);
 float** matrix_scalar_product(float **array, int scalar);

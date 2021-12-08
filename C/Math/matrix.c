3edae8ccaf90a77a15889033609ca3f18c32b3d3
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-08 00:14:04 -0300

    Refactore functions names

--- C/Math/matrix.c
+++ C/Math/matrix.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 
-float* matrix_sum(float *array1, float *array2) {
+int matrix_columns(float **array) {
+	return (sizeof(array[0]) / sizeof(float));
+}
+
+int matrix_rows(float **array) {
+	return ((sizeof(array) / sizeof(float)) / get_columns);
+}
+
+int same_order_matrices(float **array1, float **array2) {
+	if((get_columns(array1) == get_columns(array2)) \
+		&& (get_rows(array1) == get_rows(array2)))
+		return (1);
+	return (0);
 }

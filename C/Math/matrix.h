b8545d1a3f7ad03db93d15c745c9c1f005f8f60a
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-07 23:26:08 -0300

    Add matrix operations header

---
+++ C/Math/matrix.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+float* matrix_sum(float *array1, float *array2);
+float* matrix_scalar_product(float *array1, int scalar);
+float* matrix_transposition(float *array);
+float* matrix_product(float *array1, float *array2);

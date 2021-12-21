8c5efe7ab162860817108bd888d74b9f1c889d6a
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-21 10:33:59 -0300

    Refactor variable names and implement swapping rows function

--- C/Math/matrix.c
+++ C/Math/matrix.c
@@ -2,12 +2,24 @@
 #include <stdarg.h>
 #include "matrix.h"
 
-void print_matrix(int row, int column, float *array) {
-	for(size_t i = 0; i < row; ++i) {
+void print_matrix(int rows, int columns, float *array) {
+	for(size_t i = 0; i < rows; ++i) {
 		printf("| ");
-		for(size_t j = 0; j < column; ++j) {
-			printf("%.1f ", array[j + (column * i)]);
+		for(size_t j = 0; j < columns; ++j) {
+			printf("%.1f ", array[j + (columns * i)]);
 		}
 		printf("|\n");
+	}
+}
+
+void swap_rows(int rows, int columns, int upper_row,\
+				int lower_row, float* array) {
+	float temporary[columns];
+		
+	for(size_t j = 0; j < columns; ++j) {
+		temporary[j] = array[(upper_row * columns) + j];
+		array[(upper_row * columns) + j] = \
+			array[(lower_row * columns) + j];
+		array[(lower_row * columns) + j] = temporary[j];
 	}
 }

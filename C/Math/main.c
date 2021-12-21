b5afc7764e3c31cb670437bbfd56145e94989a1d
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-21 00:41:35 -0300

    Turns rows and columns into variables

--- C/Math/main.c
+++ C/Math/main.c
@@ -3,14 +3,14 @@
 #include "matrix.c"
 
 int main () {
-
-	float array[3][5] = {
+	int rows = 3;
+	int columns = 5;
+	float array[rows][columns] = {
 			{1, 2, 3, 4, 5},
 			{5, 6, 7, 8, 9},
 			{3, 4, 5, 6, 7} };
 	float *array_pointer = &array[0][0];
 
-//	printf("%.1f\n", array_pointer[5]);
-	print_matrix(3, 5, array_pointer);
+	print_matrix(rows, columns, array_pointer);
 	return (0);
 }

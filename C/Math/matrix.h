93114c20ad648bf5ffb9ef55511ab12e0b5a8453
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-21 00:41:06 -0300

    Resume print_matrix() definition

--- C/Math/matrix.h
+++ C/Math/matrix.h
@@ -1,4 +1,3 @@
 #include <stdio.h>
 
-float* define_matrix(int rows, int number_of_elements, ...);
-void print_matrix(int rows, int number_of_elements, float* matrix);
+void print_matrix(int, int, float*);

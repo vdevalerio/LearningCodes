0a6f4b0ef1eb05ff12ea8ed9378d7030d1607929
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-21 10:34:46 -0300

    Turns matrix sizes into defines statements and test swapping rows function

--- C/Math/main.c
+++ C/Math/main.c
@@ -2,15 +2,19 @@
 #include <malloc.h>
 #include "matrix.c"
 
+#define ROWS 3
+#define COLUMNS 5
+
 int main () {
-	int rows = 3;
-	int columns = 5;
-	float array[rows][columns] = {
+	float array[ROWS][COLUMNS] = {
 			{1, 2, 3, 4, 5},
 			{5, 6, 7, 8, 9},
 			{3, 4, 5, 6, 7} };
 	float *array_pointer = &array[0][0];
-
-	print_matrix(rows, columns, array_pointer);
+	printf("Original Matrix:\n");
+	print_matrix(ROWS, COLUMNS, array_pointer);
+	swap_rows(ROWS, COLUMNS, 0, 2, array_pointer);
+	printf("Swapped Matrix:\n");
+	print_matrix(ROWS, COLUMNS, array_pointer);
 	return (0);
 }

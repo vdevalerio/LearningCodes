b6748adaf498518814735f32e3be3e14be559363
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-22 00:16:20 -0300

    Add some new lines to improve readability

--- C/Math/main.c
+++ C/Math/main.c
@@ -5,16 +5,21 @@
 #define ROWS 3
 #define COLUMNS 5
 
-int main () {
+int main ()
+{
 	float array[ROWS][COLUMNS] = {
 			{1, 2, 3, 4, 5},
 			{5, 6, 7, 8, 9},
 			{3, 4, 5, 6, 7} };
 	float *array_pointer = &array[0][0];
+	
 	printf("Original Matrix:\n");
 	print_matrix(ROWS, COLUMNS, array_pointer);
+
 	swap_rows(ROWS, COLUMNS, array_pointer, 0, 2);
+
 	printf("Swapped Matrix:\n");
 	print_matrix(ROWS, COLUMNS, array_pointer);
+	
 	return (0);
 }

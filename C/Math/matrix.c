e91f50ad5cb80834d648681044818fce0781cad2
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-22 19:51:21 -0300

    Implement raise_leftmost_pivot_rows() function

--- C/Math/matrix.c
+++ C/Math/matrix.c
@@ -37,3 +37,20 @@ void swap_rows(int rows, int columns, float* array,\
 		array[(lower_row * columns) + j] = temporary[j];
 	}
 }
+
+void raise_leftmost_pivot_rows(int rows, int columns, float *array)
+{
+	size_t bigger, smaller;
+	for(size_t i = 0; i < rows; ++i)
+	{
+		bigger = get_row_pivot_index(rows, columns, array, i);
+		for(size_t j = 0; j < rows; ++j)
+		{
+			smaller = get_row_pivot_index(rows, columns, array, j);
+			if(bigger < smaller)
+			{
+				swap_rows(rows, columns, array, i, j);
+			}
+		}
+	}
+}

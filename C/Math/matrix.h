c7f3b3384408f4bc8128f19b9420143fe058f574
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-22 00:39:06 -0300

    Add function prototype for getting row pivot index

--- C/Math/matrix.h
+++ C/Math/matrix.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
 
 void print_matrix(int, int, float*);
+int get_row_pivot_index(int, int, float*, int);
+/* 	return row pivot index if row does not consist
+ * 		of only zeros
+ * 	return -1 if the row consist of only zeros
+ */ 	
 void swap_rows(int, int, float*, int, int);

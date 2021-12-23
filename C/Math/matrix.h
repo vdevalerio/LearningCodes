cedb2f8180679151b3cb0e5c9dd7c381ed99d546
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-22 22:38:52 -0300

    Add parameter names to improve readability due increasing parameters number

--- C/Math/matrix.h
+++ C/Math/matrix.h
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-void print_matrix(int, int, float*);
-int get_row_pivot_index(int, int, float*, int);
+void print_matrix(int rows, int columns, float* matrix);
+int get_row_pivot_index(int rows, int columns, float* matrix, int target_row);
 /* 	return row pivot index if row does not consist
  * 		of only zeros
  * 	return -1 if the row consist of only zeros
  */ 	
-void swap_rows(int, int, float*, int, int);
-void raise_leftmost_pivot_rows(int, int, float*);
-void multiply_row_by_nonzero(int, int, float*, int, float, float*);
+void swap_rows(int rows, int columns, float* matrix, int row1, int row2);
+void raise_leftmost_pivot_rows(int rows, int columns, float* matrix);
+void multiply_row_by_nonzero(int rows, int columns, float* matrix,\
+						int target_row, float factor, float* auxiliary_row);
 
 /* 	TODOs
  * 		adding a multiple of one row

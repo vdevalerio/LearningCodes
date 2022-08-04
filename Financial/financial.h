d495420153e72cdc27c9fd72f1bd032b29770428
Marcos Valerio <marcosvaleriox12@hotmail.com>
2022-08-03 23:05:25 -0300

    Remove financial branch, let it for future

--- Financial/financial.h
+++
@@ -1,4 +0,0 @@
-float 	simple_interest 	(float current_value, float rate, int period);
-float 	compound_interest	(float current_value, float rate, int period);
-float 	future_value		(float current_value, float rate, int period, \
-							 int type);
